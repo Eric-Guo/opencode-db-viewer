36aa22355abb6f325734955b3b1a5f69a40a6f80
Eric-Guo <eric.guocz@gmail.com>
2026-02-15 10:05:32 +0800

    Added non-FK associations

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,4 +2,5 @@ class Part < ApplicationRecord
   self.table_name = "part"
 
   belongs_to :message, class_name: "Message", foreign_key: :message_id, inverse_of: :parts
+  belongs_to :session, class_name: "Session", foreign_key: :session_id, inverse_of: :parts
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -2,8 +2,11 @@ class Session < ApplicationRecord
   self.table_name = "session"
 
   belongs_to :project, class_name: "Project", foreign_key: :project_id, inverse_of: :sessions
+  belongs_to :parent, class_name: "Session", foreign_key: :parent_id, inverse_of: :children, optional: true
 
   has_many :messages, class_name: "Message", foreign_key: :session_id, inverse_of: :session, dependent: :destroy
+  has_many :parts, class_name: "Part", foreign_key: :session_id, inverse_of: :session
+  has_many :children, class_name: "Session", foreign_key: :parent_id, inverse_of: :parent
   has_many :todos, class_name: "Todo", foreign_key: :session_id, inverse_of: :session, dependent: :destroy
   has_one :session_share,
     class_name: "SessionShare",
